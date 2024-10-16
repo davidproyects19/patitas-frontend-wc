ec07c7221502f54e71a9546912c79f71421f30c9
DAVID <fernandez.davidgoza@gmail.com>
2024-10-15 23:22:54 -0500

    Guardar proyecto

--- src/main/java/pe/edu/cibertec/patitas_frontend_wc/controller/LoginControllerAsync.java
+++ src/main/java/pe/edu/cibertec/patitas_frontend_wc/controller/LoginControllerAsync.java
@@ -3,6 +3,8 @@ package pe.edu.cibertec.patitas_frontend_wc.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
+import pe.edu.cibertec.patitas_frontend_wc.dto.LogoutResponseDTO;
+import pe.edu.cibertec.patitas_frontend_wc.dto.LogoutRequestDTO;
 import pe.edu.cibertec.patitas_frontend_wc.dto.LoginRequestDTO;
 import pe.edu.cibertec.patitas_frontend_wc.dto.LoginResponseDTO;
 import reactor.core.publisher.Mono;
@@ -21,7 +23,7 @@ public class LoginControllerAsync {
         // validar campos de entrada
         if (loginRequestDTO.tipoDocumento() == null || loginRequestDTO.tipoDocumento().trim().length() == 0 ||
                 loginRequestDTO.numeroDocumento() == null || loginRequestDTO.numeroDocumento().trim().length() == 0 ||
-                loginRequestDTO.password() == null || loginRequestDTO.password().trim().length() == 0){
+                loginRequestDTO.password() == null || loginRequestDTO.password().trim().length() == 0) {
             return Mono.just(new LoginResponseDTO("01", "Error: Debe completar correctamente sus credenciales", "", ""));
         }
 
@@ -35,7 +37,7 @@ public class LoginControllerAsync {
                     .bodyToMono(LoginResponseDTO.class)
                     .flatMap(response -> {
 
-                        if(response.codigo().equals("00")){
+                        if (response.codigo().equals("00")) {
                             return Mono.just(new LoginResponseDTO("00", "", response.nombreUsuario(), ""));
                         } else {
                             return Mono.just(new LoginResponseDTO("02", "Error: Autenticación fallida", "", ""));
@@ -43,7 +45,7 @@ public class LoginControllerAsync {
 
                     });
 
-        } catch(Exception e) {
+        } catch (Exception e) {
 
             System.out.println(e.getMessage());
             return Mono.just(new LoginResponseDTO("99", "Error: Ocurrió un problema en la autenticación", "", ""));
@@ -52,4 +54,27 @@ public class LoginControllerAsync {
 
     }
 
+    @PostMapping("/cerrar")
+    public Mono<LogoutResponseDTO> cerrarSesion(@RequestBody LogoutRequestDTO logoutRequestDTO) {
+
+        try {
+            // consumir servicio backend de
+            return webClientAutenticacion.post()
+                    .uri("/logout")
+                    .body(Mono.just(logoutRequestDTO), LogoutRequestDTO.class)
+                    .retrieve()
+                    .bodyToMono(LogoutResponseDTO.class);
+
+        } catch (Exception ex) {
+
+            System.out.println(ex.getMessage());
+            return Mono.just(new LogoutResponseDTO(false, null, "Error cerrar sesion"));
+
+        }
+
+
+    }
+
 }
+
+
